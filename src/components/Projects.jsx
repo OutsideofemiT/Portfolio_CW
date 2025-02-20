4050a1c6022ad060d039a0c37c70a86050979232
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-02-19 21:17:53 -0600

    trying to correct app.css import issue

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,6 @@ const Project = ({ title, image, deployedLink, repoLink }) => {
     <div className="project">
       <img src={image} alt={title} />
       <h3>{title}</h3>
-      <a href={deployedLink} target="_blank" rel="noopener noreferrer">View Application</a>
       <a href={repoLink} target="_blank" rel="noopener noreferrer">View GitHub Repository</a>
     </div>
   );
