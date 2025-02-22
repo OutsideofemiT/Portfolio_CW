83c2188f885a46b9545331fa813c464603760324
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-02-21 21:35:39 -0600

    resolving commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home"; // Home is inside components
@@ -6,9 +7,20 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 import "./assets/styles/App.css"; // Correct import path for App.css
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import AboutMe from "./pages/AboutMe";
+import Projects from "./components/Projects";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+>>>>>>> Stashed changes
 
-const App = () => {
+function App() {
   return (
+<<<<<<< Updated upstream
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -18,7 +30,21 @@ const App = () => {
         <Route path="resume" element={<Resume />} />
       </Route>
     </Routes>
+=======
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<AboutMe />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="resume" element={<Resume />} />
+        </Route>
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
   );
-};
+}
 
 export default App;
