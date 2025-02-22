4971082f8b71b648d60088b46330e914e98d1e28
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-02-21 20:19:33 -0600

    updating main.jsx file with global react import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-//import "bootstrap/dist/css/bootstrap.min.css"; // ⚠️ Comment this out for now
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
